b644812bcbb22a1e1b5b9252a5207ee2dc792f4a
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-01-14 23:02:18 +0100

    Update guest.routes.js

--- server/routes/guest.routes.js
+++ server/routes/guest.routes.js
@@ -6,7 +6,7 @@ const router = new Router();
 // Get all Guests
 router.route('/guests').get(GuestController.getGuests);
 
-// Edit a dedication by id
+// Edit a guest by id
 router.route('/guests/:id').put(GuestController.editGuest);
 
 
