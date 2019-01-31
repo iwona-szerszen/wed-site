8d8a612c51c78fbe05ccb9c3a2105f4d86ab1944
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-01-31 22:30:29 +0100

    Update Countdown component

--- client/components/presentational/Home/Home.js
+++ client/components/presentational/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Counter from './Counter/Counter';
+import CountdownContainer from '../../containers/CountdownContainer';
 import Carousel from './Carousel/Carousel';
 
 const Home = props => {
 	return (
 		<div>
 			<Counter />
+			<CountdownContainer weddingDate={new Date(2019, 5, 8, 17, 0, 0)} />
 			<Carousel />
 		</div>
 	);
