4bb2a05b0c9e57d8ece483a66fb4daef4a0c70a4
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-02-06 18:34:17 +0100

    Add basic styles for Present component

--- client/components/presentational/Presents/Present.js
+++ client/components/presentational/Presents/Present.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Present.css';
 
 const Present = props => {
 	return (
 		<div>
-			<img src={props.present.image} />
+			<img
+				src={require(`../../../../assets/images/${props.present.image}`)}
+				alt={props.present.image.substring(0, props.present.image.length - 4)}
+			/>
 			<h4>{props.present.name}</h4>
 			<p>{props.present.price} \u20AC</p>
 		</div>
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var path = require('path');
 var ExternalsPlugin = require('webpack2-externals-plugin');
 
+var babelLoaderExcludeNodeModulesExcept = require('babel-loader-exclude-node-modules-except');
+
 module.exports = {
   entry: path.resolve(__dirname, 'server/server.js'),
 
