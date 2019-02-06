ced73310cf2963925787123143d403a5256710b0
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-02-06 18:20:12 +0100

    Update components related to Dedications menu tab

---
+++ client/components/containers/DedicationsPageContainer.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link, connect } from 'react-redux';
+
+// Import Components
+import DedicationList from '../presentational/Dedications/DedicationList';
+
+// Import Actions
+import { fetchDedicationsRequest } from '../../actions/DedicationActions';
+
+// Import Selector
+import { getDedications } from '../../reducers/DedicationReducer';
+
+import { testGuestId } from '../../../server/config';
+
+class DedicationsPageContainer extends Component {	
+  componentDidMount() {
+    this.props.dispatch(fetchDedicationsRequest());
+  }
+  moveToAddDedication() {
+    return;
+  }
+  render() {
+  	return (
+  		<div>
+        <button className='btn btn-outline-info' onClick={this.moveToAddDedication.bind(this)}>Add dedication</button>
+  			<DedicationList dedications={this.props.dedications} />
+  		</div>
+  	);
+  }
+};
+
+// Actions required to provide data for this component to render in server side
+DedicationsPageContainer.need = [() => { return fetchDedicationsRequest(); }];
+
+// Retrieve data from store as props
+function mapStateToProps(state) {
+  return {
+    dedications: getDedications(state),
+  };
+}
+
+DedicationsPageContainer.propTypes = {
+	dedications: PropTypes.array,
+	dispatch: PropTypes.func,
+};
+
+DedicationsPageContainer.contextTypes = {
+  router: PropTypes.object,
+};
+
+export default connect(mapStateToProps)(DedicationsPageContainer);
+
+/*
+<Link to={`/guests/${testGuestId}`>
+  <button onClick={this.moveToAddDedication.bind(this)}>Add dedication</button>
+</Link>
+*/
+
+//moveToAddDedication(dispatch(changeActiveTab(tab)))
+
+/*
+  moveToAddDedication() {
+    this.context.router.push({
+      pathname: `/guests/${testGuestId}`,
+      state: {activeTabKey: 'dedications'},
+    });
+  }
+*/
--- client/components/presentational/Dedications/DedicationList.js
+++ client/components/presentational/Dedications/DedicationList.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-const Dedication = props => {
-	return (
-		<li>
-			<div>`Song: ${props.dedication.song}`</div>
-			<div>{props.dedication.content}</div>
-		</li> 
-	);
-};
-
-Dedication.propTypes = {
-	dedication: PropTypes.object,
-};
+import Dedication from './Dedication';
 
 const DedicationList = props => {
 	return (
-		<ul>
-			{props.dedications.map(item => <Dedication key={item.id} dedication={item} />)}
+		<ul className='container'>
+			{props.dedications.map(item => <Dedication key={item._id} dedication={item} />)}
 		</ul>
 	);
 };
@@ -25,7 +13,5 @@ const DedicationList = props => {
 DedicationList.propTypes = {
 	dedications: PropTypes.array,
 };
-
-//const DedicationList = props => <h2>Here will be DedicationList</h2>;
 
 export default DedicationList;
