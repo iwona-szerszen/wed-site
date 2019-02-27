2b695444b60548d36c3c028e03f079a2cdd6fd87
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-02-27 23:03:33 +0100

    Update styles of Footer component

--- client/components/presentational/AppTemplate/Footer/Footer.js
+++ client/components/presentational/AppTemplate/Footer/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import styles from './Footer.css';
-import bg from '../../../../../assets/images/gold.jpg';
 
 const Footer = () => {
 	return (
-		<footer style={{ background: `#fff url(${bg}) 0/cover` }} className={styles.footer}>
+		<footer>
 			Copyright &copy; 2019 &#124; <a href='https://github.com/iwona-szerszen' target='_blank'>Iwona Szerszen</a>
     	</footer>
 	);
