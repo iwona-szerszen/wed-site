f501a6c980d26111c5142db4644ca3f37339cf8a
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-02-27 22:57:58 +0100

    Update syntax of Navigation component

--- client/components/presentational/AppTemplate/Navigation/Navigation.js
+++ client/components/presentational/AppTemplate/Navigation/Navigation.js
@@ -1,55 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Navbar, Nav } from 'react-bootstrap';
 import { IndexLink } from 'react-router';
-
 import { testGuestId } from '../../../../../server/config';
-
 import styles from './Navigation.css';
-import bg from '../../../../../assets/images/gold.jpg';
 
-const Navigation = props => {
+const Navigation = () => {
 	return (
-		<div style={{ background: `#fff url(${bg}) 0/cover` }} className={styles.navContainer}>
-			<nav className={styles.navigation}>
-				<div>
-					<IndexLink
-						to='/' 
-						activeClassName={styles.active}
-						className={styles.link}
-					>
-						Home
-					</IndexLink>
-					<IndexLink
-						to='/guests'
-						activeClassName={styles.active}
-						className={styles.link}
-					>
-						Guests
-					</IndexLink>
-					<IndexLink
-						to='/presents'
-						activeClassName={styles.active}
-						className={styles.link} activeClassName={styles.active}
-					>
-						Presents
-					</IndexLink>
-					<IndexLink
-						to='/dedications'
-						activeClassName={styles.active}
-						className={styles.link}
-					>
-						Dedications
-					</IndexLink>
-				</div>
-				<IndexLink
-					to={`/guests/${testGuestId}`}
-					activeClassName={styles.active}
-					className={styles.yourProfile}
-				>
-					Your profile
-				</IndexLink>
-			</nav>
-		</div>
+		<nav className={styles.navContainer}>
+			<Navbar collapseOnSelect expand='sm' className={styles.navigation}>
+				<Navbar.Toggle className={`ml-auto ${styles.toggleButton}`} aria-controls='responsive-navbar-nav' />
+				<Navbar.Collapse id='responsive-navbar-nav'>
+				  	<Nav className='mr-auto'>
+						<IndexLink
+							to='/' 
+							activeClassName={styles.active}
+							className={styles.link}
+						>
+							Home
+						</IndexLink>
+						<IndexLink
+							to='/guests'
+							activeClassName={styles.active}
+							className={styles.link}
+						>
+							Guests
+						</IndexLink>
+						<IndexLink
+							to='/presents'
+							activeClassName={styles.active}
+							className={styles.link} activeClassName={styles.active}
+						>
+							Presents
+						</IndexLink>
+						<IndexLink
+							to='/dedications'
+							activeClassName={styles.active}
+							className={styles.link}
+						>
+							Dedications
+						</IndexLink>
+				    </Nav>
+				    <Nav>
+			    		<IndexLink
+							to={`/guests/${testGuestId}`}
+							activeClassName={styles.active}
+							className={styles.yourProfile}
+						>
+							Your profile
+						</IndexLink>
+				    </Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</nav>
 	);	
 };
 
@@ -61,20 +64,3 @@ export default Navigation;
 //<IndexLink to=`/guests/${id}` params={{ id: testGuestId }}>Your profile</IndexLink>
 //<IndexLink to='/guests/5c3f3af3c88fdf27d4522134'>Your profile</IndexLink>
 //<IndexLink to={`/guests/${props.userId}`}>Your profile</IndexLink>
-
-/*
-
-		<div className='container'>
-			<nav>
-				<IndexLink to='/'>Home</IndexLink>
-				<IndexLink to='/guests'>Guests</IndexLink>
-				<IndexLink to='/presents'>Presents</IndexLink>
-				<IndexLink to='/dedications'>Dedications</IndexLink>
-				<IndexLink to={`/guests/${testGuestId}`}>Your profile</IndexLink>
-			</nav>
-			<main>
-				{props.children}
-			</main>
-		</div>
-
-*/
