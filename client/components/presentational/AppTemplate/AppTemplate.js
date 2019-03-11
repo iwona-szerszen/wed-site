70b27cee29e5c9e0aa545a2dba0bc811930a3734
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-03-11 20:54:56 +0100

    Update components of AppTemplate

--- client/components/presentational/AppTemplate/AppTemplate.js
+++ client/components/presentational/AppTemplate/AppTemplate.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import Navigation from './Navigation/Navigation';
@@ -13,22 +13,19 @@ if (process.env.NODE_ENV === 'development') {
 const AppTemplate = props => {
 	return (
 		<div>
-			
-			<div>
-				<Helmet
-					title='Wedding Site'
-					meta={[
-						{ charset: 'utf-8' },
-						{ 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-			            { name: 'viewport', content: 'width=device-width, initial-scale=1' },			            
-					]}
-				/>
-				<Navigation />
-				<div>
-					{props.children}
-				</div>
-				<Footer />
-			</div>
+			<Helmet
+				title='Wedding Site'
+				meta={[
+					{ charset: 'utf-8' },
+					{ 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+		            { name: 'viewport', content: 'width=device-width, initial-scale=1' },			            
+				]}
+			/>
+			<Navigation />
+			<main>
+				{props.children}
+			</main>
+			<Footer />
 		</div>
 	);
 };
@@ -38,30 +35,3 @@ AppTemplate.propTypes = {
 }
 
 export default AppTemplate;
- 
-/*
-  render() {
-    return (
-      <div>
-        {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
-        <div>
-
-
-          <Header
-            switchLanguage={lang => this.props.dispatch(switchLanguage(lang))}
-            intl={this.props.intl}
-            toggleAddPost={this.toggleAddPostSection}
-          />
-          <div className={styles.container}>
-            {this.props.children}
-          </div>
-          <Footer />
-        </div>
-      </div>
-    );
-  }
-}
-*/
-
-
-//{process.env.NODE_ENV === 'development' && !window.devToolsExtension && <DevTools />}
--- client/components/presentational/AppTemplate/Navigation/Navigation.js
+++ client/components/presentational/AppTemplate/Navigation/Navigation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Navbar, Nav } from 'react-bootstrap';
 import { IndexLink } from 'react-router';
 import { testGuestId } from '../../../../../server/config';
@@ -57,10 +56,3 @@ const Navigation = () => {
 };
 
 export default Navigation;
-
-//<IndexLink to={`/posts/${props.post.slug}-${props.post.cuid}`} >
-//<IndexLink className='logo' to={`/countries/country/${country.id}`}>
-//<IndexLink to='/guests/5c3f3af3c88fdf27d4522134'>Your profile</IndexLink>
-//<IndexLink to=`/guests/${id}` params={{ id: testGuestId }}>Your profile</IndexLink>
-//<IndexLink to='/guests/5c3f3af3c88fdf27d4522134'>Your profile</IndexLink>
-//<IndexLink to={`/guests/${props.userId}`}>Your profile</IndexLink>
