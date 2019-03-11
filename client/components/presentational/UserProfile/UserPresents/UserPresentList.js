013c3590d1b8ec819074e3b8e8344ad1c4dcd96a
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-03-11 20:43:45 +0100

    Update UserPresentList component

--- client/components/presentational/UserProfile/UserPresents/UserPresentList.js
+++ client/components/presentational/UserProfile/UserPresents/UserPresentList.js
@@ -1,45 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router';
+import styles from '../../Presents/PresentList/PresentList.css';
+import userStyles from '../UserProfile.css';
 
-const UserPresent = props => {
+const UserPresentCard = props => {
 	return (
-		<div>
+		<div className={styles.presentCard}>
 			<img
-				//src={require(`../../../../../assets/images/${props.present.image}`)}
+				src={require(`../../../../../assets/images/${props.present.image}`)}
 				alt={props.present.image.substring(0, props.present.image.length - 4)}
 			/>
-			<h4>{props.present.name}</h4>
-			<p>{props.present.price} \u20AC</p>
+			<div className={styles.cardContent}>
+				<div className={styles.name}>{props.present.name}</div>
+				<div className={styles.price}>{props.present.price} &#8364;</div>
+				<div className={styles.buttonContainer}>
+					<Button
+						variant='info'
+						className={styles.cancelButton}
+						onClick={() => props.cancelPresentReservation(props.present._id)}
+					>
+						Cancel reservation
+					</Button>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-UserPresent.propTypes = {
+UserPresentCard.propTypes = {
 	present: PropTypes.object,
+	cancelPresentReservation: PropTypes.func,
 };
 
 const UserPresentList = props => {
 	if (!props.userPresents.length) {
 		return (
-			<div>
-				<div>You have no present reserved.</div>
-				<div>If You want to reserve present, please visit <Link to='/presents'>Presents</Link>.</div>
+			<div className={userStyles.noItemInfo}>
+				You have no present reserved.<br />If you want to reserve present, please visit <Link to='/presents'>Presents</Link> .
 			</div>
 		);
 	} else {
 		return (
-			<ul>
+			<div className={styles.userPresentList}>
 				{props.userPresents.map(item => (
-					<li key={item._id}>
-						<UserPresent
-							key={item._id}
-							present={item}
-						/>
-						<button	className='btn btn-info' onClick={() => props.cancelPresentReservation(item._id)}>Cancel reservation</button>
-					</li>
+					<UserPresentCard
+						key={item._id}
+						present={item}
+						cancelPresentReservation={props.cancelPresentReservation}
+					/>
 				))}
-			</ul>
+			</div>
 		);
 	}
 };
