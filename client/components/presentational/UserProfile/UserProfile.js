40ed2ded5d282e340cc2b434f80e9200dc0edcee
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-03-11 20:30:41 +0100

    Add styles to UserProfile component

--- client/components/presentational/UserProfile/UserProfile.js
+++ client/components/presentational/UserProfile/UserProfile.js
@@ -1,62 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Tab, Row, Col, Nav } from 'react-bootstrap';
+import { Tab, Nav } from 'react-bootstrap';
 import UserConfirmation from './UserConfirmation/UserConfirmation';
 import UserPresentList from './UserPresents/UserPresentList';
 import AddDedication from './UserDedications/AddDedication';
 import UserDedicationList from './UserDedications/UserDedicationList';
-
+import styles from './UserProfile.css';
+	
 const UserProfile = props => {
 	return (
-		<Tab.Container 
-			id='user-tabs'
-			activeKey={props.activeTabKey}
-			onSelect={key => props.handleChangeTab(key)}
-		>
-			<Row>
-			    <Col sm={3}>
-			      	<Nav variant='pills' className='flex-column'>
-			        	<Nav.Item>
-			        		<Nav.Link eventKey='confirmation'>Presence confirmation</Nav.Link>
-			        	</Nav.Item>
-			        	<Nav.Item>
-			        		<Nav.Link eventKey='presents'>Reserved presents</Nav.Link>
-			        	</Nav.Item>
-			        	<Nav.Item>
-			        		<Nav.Link eventKey='dedications'>Music dedications</Nav.Link>
-			        	</Nav.Item>
-			      	</Nav>
-			    </Col>
-			    <Col sm={9}>
-			      	<Tab.Content>
-			        	<Tab.Pane eventKey='confirmation'>
-			        		<UserConfirmation
-			  					user={props.guest}
-			  					presenceForm={props.presenceForm}
-			  					onChangePresenceInput={props.handleChangePresenceInput}
-			  					onSubmitConfirmPresence={props.handleSubmitConfirmPresence}
-			  					//onSubmitEditConfirmation={props.handleSubmitEditConfirmation}
-			  				/>
-			        	</Tab.Pane>
-			        	<Tab.Pane eventKey='presents'>
-			        		<UserPresentList
-			  					userPresents={props.guest.presents}
-			  					cancelPresentReservation={props.handleCancelPresentReservation}
-			  				/>
-			        	</Tab.Pane>
-			        	<Tab.Pane eventKey='dedications'>
-					  		<AddDedication
-					  			onSubmitAddDedication={props.handleSubmitAddDedication}
-					  		/>
-					  		<UserDedicationList
-					  			userDedications={props.guest.dedications}
-					  			deleteDedication={props.handleDeleteDedication}
-					  		/>
-			        	</Tab.Pane>			        	
-			      	</Tab.Content>
-			    </Col>
-			</Row>
-		</Tab.Container>
+		<div className={styles.containerFluid}>
+				<Tab.Container
+					id='user-tabs'
+					activeKey={props.activeTabKey}
+					onSelect={key => props.handleChangeTab(key)}
+				>
+					<div className={styles.container}>
+				      	<Nav variant='pills' className={styles.tabNavigation}>
+				        	<Nav.Item>
+				        		<Nav.Link eventKey='confirmation'>Presence confirmation</Nav.Link>
+				        	</Nav.Item>
+				        	<Nav.Item>
+				        		<Nav.Link eventKey='presents'>Reserved presents</Nav.Link>
+				        	</Nav.Item>
+				        	<Nav.Item>
+				        		<Nav.Link eventKey='dedications'>Music dedications</Nav.Link>
+				        	</Nav.Item>
+				      	</Nav>
+				      	<Tab.Content>
+				        	<Tab.Pane eventKey='confirmation'>
+				        		<UserConfirmation
+				  					user={props.guest}
+				  					presenceForm={props.presenceForm}
+				  					onChangePresenceInput={props.handleChangePresenceInput}
+				  					onSubmitConfirmPresence={props.handleSubmitConfirmPresence}
+				  				/>
+				        	</Tab.Pane>
+				        	<Tab.Pane eventKey='presents'>
+				        		<UserPresentList
+				  					userPresents={props.guest.presents}
+				  					cancelPresentReservation={props.handleCancelPresentReservation}
+				  				/>
+				        	</Tab.Pane>
+				        	<Tab.Pane eventKey='dedications'>
+						  		<AddDedication
+						  			onSubmitAddDedication={props.handleSubmitAddDedication}
+						  		/>
+						  		<hr className={styles.dividingLine} />
+						  		<UserDedicationList
+						  			userDedications={props.guest.dedications}
+						  			deleteDedication={props.handleDeleteDedication}
+						  		/>
+				        	</Tab.Pane>			        	
+				      	</Tab.Content>
+					</div>
+				</Tab.Container>
+			
+		</div>
 	);
 };
 
@@ -67,7 +67,6 @@ UserProfile.propTypes = {
 	handleChangeTab: PropTypes.func,
 	handleChangePresenceInput: PropTypes.func,
 	handleSubmitConfirmPresence: PropTypes.func,
-	handleSubmitEditConfirmation: PropTypes.func,
 	handleSubmitAddDedication: PropTypes.func,
 	handleCancelPresentReservation: PropTypes.func,
 	handleDeleteDedication: PropTypes.func,
