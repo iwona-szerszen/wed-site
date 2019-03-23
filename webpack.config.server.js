f9985f3ecd193bb353775fb056a202cbcd0d5c3c
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-03-23 13:39:16 +0100

    Update config files by adding babel-preset-env

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -35,7 +35,7 @@ module.exports = {
           options: {
             presets: [
               'react',
-              'es2015',
+              'env',
               'stage-0',
             ],
             plugins: [
